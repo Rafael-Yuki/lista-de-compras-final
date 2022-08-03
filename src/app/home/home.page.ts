47c5e44855b1fe22ab4e30fcc8241f0eee3dbb22
rafael-yuki <rafael.tobaru@edu.unifil.br>
2022-08-03 19:20:58 -0300

    backup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,18 @@ export class HomePage {
 
   constructor() {}
 
+  variavel_lista : string[] = [];
+  texto = ""
+
+  adiciona() {
+    if (!(this.texto == "")) {
+      this.variavel_lista.push(this.texto);
+      this.texto = "";
+    }
+
+  }
+
+  //*ngFor = "let ___ of _______" ex: *ngFor = "let item of variavel_lista"
+  //[(ngModel)]="texto" -> input
+
 }
